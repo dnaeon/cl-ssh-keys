4b29568f3071647ceefe030f4fd66176f12efe26
Marin Atanasov Nikolov <dnaeon@gmail.com>
2020-08-06 14:02:55 +0300

    Commit implementation for RFC4251:ENCODE :public-key values

--- src/common.lisp
+++ src/common.lisp
@@ -64,34 +64,26 @@ from the binary stream and then dispatched to the respective implementation."
        (error 'invalid-key-error
               :description (format nil "Unknown key type ~a" key-type-name))))))
 
-(defun parse-public-key (text)
-  "Parses an OpenSSH public key file from the given plain-text string"
-  (let* ((parts (uiop:split-string text :separator '(#\Space)))
-         (key-type (get-key-type (first parts) :by :name))
-         (data (second parts))
-         (comment (third parts)))
-    ;; A key type identifier is expected
-    (unless key-type
-      (error 'invalid-key-error
-             :description "Invalid key: missing or unknown key type"))
-
-    ;; OpenSSH public keys are encoded in a way, so that the
-    ;; key kind preceeds the actual public key components.
-    ;; See RFC 4253 for more details.
-    (let* ((stream (rfc4251:make-binary-input-stream (binascii:decode-base64 data)))
-           (key-type-name (getf key-type :name))
-           (encoded-key-type-name (rfc4251:decode :string stream)))
-      (unless (string= key-type-name encoded-key-type-name)
-        (error 'key-type-mismatch-error
-               :description "Invalid key: key types mismatch"
-               :expected key-type-name
-               :found encoded-key-type-name))
-      (rfc4251:decode :public-key stream :key-type-name key-type-name :comment comment))))
-
-(defun parse-public-key-from-file (path)
-  "Parses an OpenSSH public key from the given path"
-  (with-open-file (in path)
-    (parse-public-key (read-line in))))
+(defmethod rfc4251:encode ((type (eql :public-key)) (key base-public-key) stream &key (encode-key-type-p t))
+  "Encodes the public key into the binary stream according to RFC 4253, section 6.6.
+If ENCODE-KEY-TYPE-P is T, then the key type name (e.g. ssh-rsa) is
+encoded in the stream as well, before the actual public key components.
+Some key types (e.g. OpenSSH certificate keys) do not encode the key
+type name, when being embedded within a certificate."
+  (let* ((kind (key-kind key))
+         (key-id (getf kind :id))
+         (key-type-name (getf kind :plain-name)))
+    ;; The number of bytes written should be the sum of the
+    ;; key-type name and the public key components
+    (+
+     (if encode-key-type-p
+         (rfc4251:encode :string key-type-name stream)
+         0) ;; No key type name being encoded, so return 0 here.
+     (case key-id
+       (:ssh-rsa (rfc4251:encode :rsa-public-key key stream))
+       (t
+        (error 'invalid-key-error
+               :description (format nil "Unknown key type ~a" key-type-name)))))))
 
 (defmethod fingerprint ((hash-spec (eql :md5)) (key base-public-key) &key)
   "Computes the MD5 fingerprint of the given public key"
@@ -138,3 +130,32 @@ from the binary stream and then dispatched to the respective implementation."
   "Computes the SHA-256 fingerprint of the embedded public key"
   (with-slots (public-key) key
     (fingerprint :sha256 public-key)))
+
+(defun parse-public-key (text)
+  "Parses an OpenSSH public key file from the given plain-text string"
+  (let* ((parts (uiop:split-string text :separator '(#\Space)))
+         (key-type (get-key-type (first parts) :by :name))
+         (data (second parts))
+         (comment (third parts)))
+    ;; A key type identifier is expected
+    (unless key-type
+      (error 'invalid-key-error
+             :description "Invalid key: missing or unknown key type"))
+
+    ;; OpenSSH public keys are encoded in a way, so that the
+    ;; key kind preceeds the actual public key components.
+    ;; See RFC 4253 for more details.
+    (let* ((stream (rfc4251:make-binary-input-stream (binascii:decode-base64 data)))
+           (key-type-name (getf key-type :name))
+           (encoded-key-type-name (rfc4251:decode :string stream)))
+      (unless (string= key-type-name encoded-key-type-name)
+        (error 'key-type-mismatch-error
+               :description "Invalid key: key types mismatch"
+               :expected key-type-name
+               :found encoded-key-type-name))
+      (rfc4251:decode :public-key stream :key-type-name key-type-name :comment comment))))
+
+(defun parse-public-key-from-file (path)
+  "Parses an OpenSSH public key from the given path"
+  (with-open-file (in path)
+    (parse-public-key (read-line in))))
