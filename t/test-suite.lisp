148751d8c186069ba9951aebd2451c10c59bef08
Marin Atanasov Nikolov <dnaeon@gmail.com>
2020-08-07 12:23:50 +0300

    tests: commit tests for private keys

--- t/test-suite.lisp
+++ t/test-suite.lisp
@@ -59,6 +59,32 @@
       (ok (string= (ssh-keys:key-comment key) "john.doe@localhost")
           "RSA 1024-bit key comment")))
 
+  (testing "RSA 1024-bit private key"
+    (let ((key (ssh-keys:parse-private-key-from-file (get-test-key-path #P"id_rsa_1024"))))
+      ;; Fingerprints of private keys are computed against the embedded public key
+      (ok (string= (ssh-keys:fingerprint :md5 key)
+                   "dd:e6:24:29:55:48:40:af:28:2c:68:f3:33:40:58:20")
+          "RSA 1024-bit private key MD5 fingerprint")
+      (ok (string= (ssh-keys:fingerprint :sha1 key)
+                   "6K8c+b+HKqUjfIcT6WbQhoEjQM0")
+          "RSA 1024-bit private key SHA-1 fingerprint")
+      (ok (string= (ssh-keys:fingerprint :sha256 key)
+                   "jVI0ipXN04yjWVqOo4jjdS3ndneErRTHXKJUDbXjg18")
+          "RSA 1024-bit private key SHA-256 fingerprint")
+      (ok (equal (ssh-keys:key-kind key)
+                 '(:name "ssh-rsa" :plain-name "ssh-rsa" :short-name "RSA" :id :ssh-rsa :is-cert nil))
+          "RSA 1024-bit key kind")
+      (ok (= (ssh-keys:key-bits key) 1024)
+          "RSA 1024-bit key number of bits")
+      (ok (string= (ssh-keys:key-comment key) "john.doe@localhost"
+          "RSA 1024-bit key comment"))
+      (ok (string= (ssh-keys:key-cipher-name key) "none")
+          "RSA 3072-bit private key cipher name")
+      (ok (string= (ssh-keys:key-kdf-name key) "none")
+          "RSA 3072-bit private key KDF name")
+      (ok (equalp (ssh-keys:key-kdf-options key) #())
+          "RSA 3072-bit private key KDF options")))
+
   (testing "RSA 3072-bit public key"
     (let ((key (ssh-keys:parse-public-key-from-file (get-test-key-path #P"id_rsa_3072.pub"))))
       (ok (string= (ssh-keys:fingerprint :md5 key)
@@ -76,7 +102,33 @@
       (ok (= (ssh-keys:key-bits key) 3072)
           "RSA 3072-bit key number of bits")
       (ok (string= (ssh-keys:key-comment key) "john.doe@localhost")
-          "RSA 3072-bit key comment"))))
+          "RSA 3072-bit key comment")))
+
+  (testing "RSA 3072-bit private key"
+    (let ((key (ssh-keys:parse-private-key-from-file (get-test-key-path #P"id_rsa_3072"))))
+      ;; Fingerprints of private keys are computed against the embedded public key
+      (ok (string= (ssh-keys:fingerprint :md5 key)
+                   "04:02:4b:b2:43:39:a4:8e:89:47:49:6f:30:78:94:1e")
+          "RSA 3072-bit private key MD5 fingerprint")
+      (ok (string= (ssh-keys:fingerprint :sha1 key)
+                   "RnLPLG93GrABjOqc6xOvVFpQXsc")
+          "RSA 3072-bit private key SHA-1 fingerprint")
+      (ok (string= (ssh-keys:fingerprint :sha256 key)
+                   "VmYpd+5gvA5Cj57ZZcI8lnFMNNic6jpnnBd0WoNG1F8")
+          "RSA 3072-bit private key SHA-256 fingerprint")
+      (ok (equal (ssh-keys:key-kind key)
+                 '(:name "ssh-rsa" :plain-name "ssh-rsa" :short-name "RSA" :id :ssh-rsa :is-cert nil))
+          "RSA 3072-bit key kind")
+      (ok (= (ssh-keys:key-bits key) 3072)
+          "RSA 3072-bit key number of bits")
+      (ok (string= (ssh-keys:key-comment key) "john.doe@localhost")
+          "RSA 3072-bit key comment")
+      (ok (string= (ssh-keys:key-cipher-name key) "none")
+          "RSA 3072-bit private key cipher name")
+      (ok (string= (ssh-keys:key-kdf-name key) "none")
+          "RSA 3072-bit private key KDF name")
+      (ok (equalp (ssh-keys:key-kdf-options key) #())
+          "RSA 3072-bit private key KDF options"))))
 
 (deftest invalid-keys
   (ok (signals (ssh-keys:parse-public-key-from-file (get-test-key-path #P"id_rsa_unknown_key_type.pub")))
