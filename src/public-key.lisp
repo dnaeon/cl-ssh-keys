6ac8044f39f9283e2c88c7db201609d65542d947
Marin Atanasov Nikolov <dnaeon@gmail.com>
2021-06-28 17:19:50 +0300

    Support decoding an RSA certificate key

--- src/public-key.lisp
+++ src/public-key.lisp
@@ -58,6 +58,7 @@ from the binary stream and then dispatched to the respective implementation."
           (:ecdsa-sha2-nistp256 (rfc4251:decode :ecdsa-nistp256-public-key stream :kind key-type :comment comment))
           (:ecdsa-sha2-nistp384 (rfc4251:decode :ecdsa-nistp384-public-key stream :kind key-type :comment comment))
           (:ecdsa-sha2-nistp521 (rfc4251:decode :ecdsa-nistp521-public-key stream :kind key-type :comment comment))
+	  (:ssh-rsa-cert-v01 (rfc4251:decode :ssh-cert-key stream :kind key-type :comment comment))
           (t
            (error 'invalid-key-error
                   :description (format nil "Unknown key type ~a" key-type-name))))
